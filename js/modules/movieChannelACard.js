e196c06e7479805880b4cc625f53699988c2bd74
Juna <71682432+Junhwa1207-design@users.noreply.github.com>
2020-12-16 01:23:50 -0500

    added js

---
+++ js/modules/movieChannelACard.js
@@ -0,0 +1,21 @@
+export default { 
+    Name: "movieChannelACard ",
+    components: ["movieChannelAcard "],
+
+data: function() { return { myName: this.movie.name, myYear: this.movie.year,
+myDescription: this.movie.description, program: "IDP", removeAformat: true,
+showBioData: false } }, 
+template: `
+<li @click="logClicked">
+        <img :src="'imagesC-A/' + movie.image" :alt='movie.name + " image"'>
+        
+        <p class=" remove-movie">{{ movie.name }}</p>
+
+        <!-- <a href="" class="remove-movie">More Info {{ movie.name }}'s info</a>                 
+        <a href="" class="remove-movie">Remove {{ movie.name }}</a>-->
+    </li>
+`, 
+created: function () { console.log(`created ${this.movie.name}'s card`); },
+
+methods: { logClicked() { console.log(`fired from inside ${this.movie.name}'s
+component!`); this.$emit("showmydata", this.movie) } } }
