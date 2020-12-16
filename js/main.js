4c9667324363cdffe6ac1e2fd8e7bb564255961f
Juna <71682432+Junhwa1207-design@users.noreply.github.com>
2020-12-16 07:17:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { fetchData} from "./modules/TheDataMiner.js";
-import { SendMail } from "./modules/mailer.js";
-import movieChannelACard from "./modules/movieChannelACard.js";
-
+import { SendMail } from "./modules/mail/mailer.js";
+import channelACard from "./modules/channelACard.js";
+//import channelKCard from "./modules/channelKCard.js";
 //import movieChannelACard from "./modules/header.js";
 
 
@@ -13,21 +13,29 @@ const myVM =(()=> {
             // anotherMessage: "Find your 3 Doors Mini.",
             removeAformat: true,
             showBioData: false,
-            movieChannelA: [ ],
-            currentMovieChannelAData:{}
-            
+            channelA: [ ],
+            channelK: [ ],
+            currentMovieChannelAData:{},
+            currentMovieChannelKData:{}
         },
         mounted: function(){
-            //console.log("Vue is mounted, trying a fetch for the initial data");
+            console.log("Vue is mounted, trying a fetch for the initial data");
 
             fetchData("./includes/index.php")
             .then(data => {
-            data.forEach(movieChannelA => this.movieChannelA.push(movieChannelA));
+            data.forEach(channelA => this.channelA.push(channelA));
             })
             .catch(err => console.error(err));
      },
+         mounted: function(){
+        console.log("Vue is mounted, trying a fetch for the initial data");
 
-
+        fetchData("./includes/index.php")
+        .then(data => {
+        data.forEach(channelK => this.channelK.push(channelK));
+        })
+        .catch(err => console.error(err));
+ },
      methods:{
         logclicked(){
             console.log("clicked on a list item")
@@ -39,95 +47,103 @@ const myVM =(()=> {
         showMovieData (target) {
  
             console.log('clicked to view movieChannelA bio-data', target, target.name);
-      
             this.showBioData = this.showBioData ? false : true;
-
             this. currentMovieChannelAData = target;
-        }
+        },
 
+        showMovieData (target) {
+ 
+            console.log('clicked to view movieChannelA bio-data', target, target.name);
+            this.showBioData = this.showBioData ? false : true;
+            this. currentMovieChannelAData = target;
+            this. currentMovieChannelKData = target;
+        }
     },
         components: {
-            "movieChannelA-card" : movieChannelACard      
+            "channela-card" : channelACard,  
+            "channelk-card" : channelKCard  
         }
     }).$mount("#app");
-
 })();
 
-(()=> {
 
+
+
+(() =>{
     var slides=document.querySelector('.slider-items').children;
-     var nextSlide=document.querySelector(".right-slide");
-     var prevSlide=document.querySelector(".left-slide");
-     var totalSlides=slides.length;
-     var index=0;
-    var i=slides;
+    var nextSlide=document.querySelector(".right-slide");
+    var prevSlide=document.querySelector(".left-slide");
+    var totalSlides=slides.length;
+    var index=0;
+   var i=slides;
 
-     nextSlide.onclick=function (){
-         console.log("click");
-         next("next");
-     }
-     prevSlide.onclick=function(){
+    nextSlide.onclick=function (){
         console.log("click");
-        next("prev");
+        next("next");
     }
+    prevSlide.onclick=function(){
+       console.log("click");
+       next("prev");
+   }
 
-    function next(direction){
+   function next(direction){
 
-        if(direction=="next"){
-            index++;
-            if(index==totalSlides){
-                index=0;
-            }
-        }
-        else{
-            if(index==0){
-                index=totalSlides-1;
-            }
-            else{
-                index--;
-            }
-        }
-
-       for(i=0; i<slides.length;i++){
-           slides[i].classList.remove("active");
+       if(direction=="next"){
+           index++;
+           if(index==totalSlides){
+               index=0;
+           }
        }
-       slides[index].classList.add ("active");
-    
-}
+       else{
+           if(index==0){
+               index=totalSlides-1;
+           }
+           else{
+               index--;
+           }
+       }
 
+      for(i=0; i<slides.length;i++){
+          slides[i].classList.remove("active");
+      }
+      slides[index].classList.add ("active");
+   
+}
 
 let mailSubmit = document.querySelector('.submit-wrapper');
 
-    function processMailFailure(result) {
-        // show a failure message in the UI
-        console.table(result); // table shows us an object in table form
-        alert(result.message);
+   function processMailFailure(result) {
+       //console.table(result)
+       alert(result.message);
+   }
 
-        // show some UI here to let the user know the mail attempt was successful
-    }
+   function processMailSuccess(result) {
+     
+alert(result.message);
+}
 
-    function processMailSuccess(result) {
-        // show a success message in the UI
-        console.table(result); // table shows us an object in table form
-        alert(result.message);
+   function processMail(event) {
 
-        // show some UI here to let the user know the mail attempt was successful
-    }
+       event.preventDefault();
 
-    function processMail(event) {
-        // block the default submit behaviour
-        event.preventDefault();
+       SendMail(this.parentNode)
+           .then(data => processMailSuccess(data))
+           .catch(err => processMailFailure(err));
 
-        // use the SendMail component to try to process mail
-        SendMail(this.parentNode)
-            .then(data => processMailSuccess(data))
-            .catch(err => processMailFailure(err));
+         
+   }
 
-            // the error handler in the catch block could actually be a generic catch-and-display function that handles EVERY error you might encounter during runtime. Might be a better strategy to pass in a flag or just a message and have the function display it in the UI
-    }
+   mailSubmit.addEventListener("click", processMail);
 
-    mailSubmit.addEventListener("click", processMail);
 })();
+
+
+
+  
+
+
+
+
 
 
 
